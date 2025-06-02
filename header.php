fbd3a04c8acb966eea64c7687eabe73258cd9712
AryaKopet <aryasokker23@gmail.com>
2025-06-02 19:37:47 +0700

    optimalisasi tampilan header

--- header.php
+++ header.php
@@ -14,8 +14,17 @@
     <!-- NAVBAR MODERN -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
         <div class="container">
+            <?php
+            $navbar_type = get_theme_mod('navbar_brand_type', 'title');
+            $navbar_logo = get_theme_mod('navbar_logo_image');
+            ?>
+
             <a class="navbar-brand fw-bold text-primary" href="<?php echo home_url(); ?>">
-                <?php bloginfo('name'); ?>
+                <?php if ($navbar_type === 'logo' && $navbar_logo) : ?>
+                    <img src="<?php echo esc_url($navbar_logo); ?>" alt="Logo" style="height: 40px;">
+                <?php else : ?>
+                    <?php bloginfo('name'); ?>
+                <?php endif; ?>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
                 <span class="navbar-toggler-icon"></span>
