449711f4d2c3f23731bdd00315d923c0437f0211
AryaKopet <aryasokker23@gmail.com>
2025-06-03 23:15:32 +0700

    pencarian berita dari filter

--- archive.php
+++ archive.php
@@ -1,20 +1,26 @@
 <?php get_header(); ?>
+
 <div class="container py-5">
     <h1 class="mb-4">
         Arsip: <?php the_archive_title(); ?>
     </h1>
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <div class="card mb-4">
+            <div class="card mb-4 shadow-sm border-0">
+                <!-- <?php if (has_post_thumbnail()) : ?>
+                    <img src="<?php the_post_thumbnail_url('large'); ?>" class="card-img-top" alt="<?php the_title(); ?>">
+                <?php endif; ?> -->
                 <div class="card-body">
                     <h2 class="card-title"><?php the_title(); ?></h2>
+                    <p class="text-muted small mb-2"><?php echo get_the_date(); ?></p>
                     <p class="card-text"><?php the_excerpt(); ?></p>
                     <a href="<?php the_permalink(); ?>" class="btn btn-primary">Baca Selengkapnya</a>
                 </div>
             </div>
         <?php endwhile;
-    else: ?>
+    else : ?>
         <p>Tidak ada postingan untuk kategori ini.</p>
     <?php endif; ?>
 </div>
+
 <?php get_footer(); ?>
