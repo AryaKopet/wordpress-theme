14eb7e18d3bbcce8ac9bae6b9e77ee98463e7d8b
AryaKopet <aryasokker23@gmail.com>
2025-06-04 20:41:18 +0700

    optimalisasi tampilan jadwal kegiatan gereja

--- page-jadwal.php
+++ page-jadwal.php
@@ -1,41 +1,42 @@
-<?php
-/*
-Template Name: Jadwal Ibadah
-*/
-get_header(); ?>
+<?php /* Template Name: Jadwal Ibadah */ ?>
+<?php get_header(); ?>
 
-<div class="container py-5">
-    <h1 class="mb-4">Jadwal Ibadah Mingguan</h1>
-    <table class="table table-striped table-bordered">
-        <thead class="table-dark">
-            <tr>
-                <th>Hari</th>
-                <th>Jam</th>
-                <th>Jenis Ibadah</th>
-                <th>Tempat</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Minggu</td>
-                <td>07.00</td>
-                <td>Ibadah Pagi</td>
-                <td>Gereja Utama</td>
-            </tr>
-            <tr>
-                <td>Minggu</td>
-                <td>17.00</td>
-                <td>Ibadah Sore</td>
-                <td>Gereja Utama</td>
-            </tr>
-            <tr>
-                <td>Rabu</td>
-                <td>19.00</td>
-                <td>Doa Tengah Minggu</td>
-                <td>Gedung Serbaguna</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<section class="container py-5">
+    <h1 class="mb-4 text-center">Jadwal Ibadah</h1>
+
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped align-middle text-center">
+            <thead class="table-primary">
+                <tr>
+                    <th>Hari</th>
+                    <th>Waktu</th>
+                    <th>Tempat</th>
+                    <th>Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Minggu</td>
+                    <td>07.00 WIB</td>
+                    <td>Gereja Induk</td>
+                    <td>Ibadah Umum</td>
+                </tr>
+                <tr>
+                    <td>Rabu</td>
+                    <td>18.30 WIB</td>
+                    <td>Aula Gereja</td>
+                    <td>Ibadah Doa</td>
+                </tr>
+                <tr>
+                    <td>Sabtu</td>
+                    <td>16.00 WIB</td>
+                    <td>Gereja Anak</td>
+                    <td>Ibadah Pemuda</td>
+                </tr>
+                <!-- Tambahkan baris sesuai kebutuhan -->
+            </tbody>
+        </table>
+    </div>
+</section>
 
 <?php get_footer(); ?>
